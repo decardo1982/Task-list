e10801c5f21c3a2890ff323ed3a7e133b6eed78e
Dario <dario.cardozo@hotmail.com>
2022-01-24 20:24:56 -0300

    upgrade

--- task-list/src/app/Task.ts
+++ task-list/src/app/Task.ts
@@ -1,6 +1,6 @@
 export interface Task {
-    id?: number, //Cuando creamos podria no venir 
-    text: string,
-    day: string,
+    id?: number; //Cuando creamos podria no venir 
+    text: string;
+    day: string;
     reminder: boolean;
 }
