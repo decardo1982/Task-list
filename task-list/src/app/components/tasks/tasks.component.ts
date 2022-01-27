4ccae4a8b167c1d39e7dad204c5fd0993885ce82
Dario <dario.cardozo@hotmail.com>
2022-01-26 23:51:43 -0300

    metodo agregar tareas

--- task-list/src/app/components/tasks/tasks.component.ts
+++ task-list/src/app/components/tasks/tasks.component.ts
@@ -25,13 +25,22 @@ export class TasksComponent implements OnInit {
     this.taskService.deleteTask(task)
       .subscribe(
         () => {  
-        this.tasks = this.tasks.filter(
-          (t) => {          
+        this.tasks = this.tasks.filter( (t) => {
           return t.id !== task.id
-      }
-      )
-    }
-    )
+      })
+    })
   }
+
+  toggleReminder(task:Task){
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe();
+    }
+
+    addTask(task:Task){
+      this.taskService.addTask(task).subscribe((task) => {
+        this.tasks.push(task)
+      }
+        )
+    }
 
 }
